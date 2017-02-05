491c6cf5c3752c8b72ec137dc7a426ddf9459c34
Kristian Kann <arrvi@arrvi.eu>
2017-02-05 20:50:04 +0100

    Ship chooser GUI stub

--- src/pl/edu/pja/s11531/mas/stms/gui/requests/create/ShipChooserScreen.groovy
+++ src/pl/edu/pja/s11531/mas/stms/gui/requests/create/ShipChooserScreen.groovy
@@ -3,13 +3,18 @@ package pl.edu.pja.s11531.mas.stms.gui.requests.create
 import groovy.swing.SwingBuilder
 import pl.edu.pja.s11531.mas.stms.gui.MainWindow
 import pl.edu.pja.s11531.mas.stms.gui.NamedScreen
+import pl.edu.pja.s11531.mas.stms.model.Spaceship
+import pl.edu.pja.s11531.mas.stms.persistence.LinkedObject
 
+import javax.swing.*
 import java.awt.*
 
 /**
  * Created by kris on 2/4/17.
  */
 class ShipChooserScreen extends NamedScreen {
+    private JPanel shipsPanel
+
     ShipChooserScreen(MainWindow window) {
         super(new BorderLayout())
         this.window = window
@@ -23,10 +28,59 @@ class ShipChooserScreen extends NamedScreen {
                 label "<html><big>Select ships</big>"
             }, BorderLayout.NORTH
 
+            this.add shipsPanel = panel {
+                vbox {
+                    label("Selected ships")
+                    scrollPane {
+                        builder.list()
+                    }
+                }
+            }, BorderLayout.WEST
+
             this.add builder.panel {
-                builder.button(text: "Back", actionPerformed: { window.showScreen(window.waypointChooser) })
-                builder.button(text: "Continue", actionPerformed: { window.showScreen(window.requestSummary) })
+                vbox {
+                    label("Available ships")
+                    scrollPane {
+                        builder.list(items: LinkedObject.getExtent(Spaceship.class))
+                    }
+                    hbox {
+                        vbox {
+                            hbox {
+                                label("Ship name")
+                                textField(id: 'shipName')
+                            }
+                            hbox {
+                                label("Cargo")
+                                textArea(id: 'cargo')
+                            }
+                        }
+                        vbox {
+                            hbox {
+                                label("Mass")
+                                textField(id: 'shipMass')
+                                label("+")
+                                textField(id: 'additionalMass')
+                            }
+                            hbox {
+                                label("Captain")
+                                textField(id: 'captain')
+                            }
+                            hbox {
+                                label("Owner")
+                                textField(id: 'owner')
+                            }
+                        }
+                    }
+                    hbox {
+                        builder.button(text: "Back", actionPerformed: { window.showScreen(window.waypointChooser) })
+                        builder.button(text: "Continue", actionPerformed: { window.showScreen(window.requestSummary) })
+                    }
+                }
             }, BorderLayout.CENTER
         }
+    }
+
+    private class ShipDetailsPanel extends JPanel {
+
     }
 }
